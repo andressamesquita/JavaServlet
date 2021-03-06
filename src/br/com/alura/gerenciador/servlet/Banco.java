106c2b3d46e5429bfd7a39f73d72d219f8975a74
AndressaMesquita <rib.mesq.Andressa72@gmail.com>
2021-03-05 22:38:01 -0300

    Criação de jsp

--- src/br/com/alura/gerenciador/servlet/Banco.java
+++ src/br/com/alura/gerenciador/servlet/Banco.java
@@ -7,6 +7,15 @@ public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<Empresa>();
 	
+	static {
+		Empresa empresa1 = new Empresa();
+		empresa1.setNome("Alura");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Caelum");
+		lista.add(empresa1);
+		lista.add(empresa2);
+	}
+	
 	public void adiciona(Empresa empresa) {
 		Banco.lista.add(empresa);		
 	}
--- src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -3,6 +3,7 @@ package br.com.alura.gerenciador.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,9 +27,12 @@ public class NovaEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
+		//chamar o JSP
+		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		request.setAttribute("empresa", empresa.getNome());
+		rd.forward(request, response);
 		
-		PrintWriter out = response.getWriter();
-		out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso!</body></html>");
+		
 	}
 
 }
