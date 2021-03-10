78e87437ab83135e0a1c834c8e06c8eaa7fce366
AndressaMesquita <rib.mesq.Andressa72@gmail.com>
2021-03-10 10:57:15 -0300

    Implemetação de DELETE em RemoveEmpresaServlet

--- WebContent/listaEmpresas.jsp
+++ WebContent/listaEmpresas.jsp
@@ -20,7 +20,10 @@
 	
 		<ul>
 		<c:forEach items="${empresas}" var="empresa">
-			<li>${empresa.nome} - <fmt:formatDate value="${empresa.dataAbertura}" pattern="dd/MM/yyyy"/></li>
+			<li>
+				${empresa.nome} - <fmt:formatDate value="${empresa.dataAbertura}" pattern="dd/MM/yyyy"/>
+				<a href="/gerenciador/removeEmpresa?id=${empresa.id}">remover</a>
+			</li>
 		</c:forEach>
 		</ul>
 	
--- src/br/com/alura/gerenciador/servlet/Banco.java
+++ src/br/com/alura/gerenciador/servlet/Banco.java
@@ -1,27 +1,49 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<Empresa>();
+	private static Integer chaveSequencial = 1; 
+	
 	
 	static {
 		Empresa empresa1 = new Empresa();
+		empresa1.setId(chaveSequencial++);
 		empresa1.setNome("Alura");
+		
 		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
 		empresa2.setNome("Caelum");
+		
 		lista.add(empresa1);
 		lista.add(empresa2);
 	}
 	
 	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
 		Banco.lista.add(empresa);		
 	}
 	
 	public List<Empresa> getEmpresas(){
 		return Banco.lista;
+	}
+
+	public void removeEmpresa(Integer id){
+		
+		Iterator<Empresa> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			if (emp.getId() == id) {
+				it.remove();
+			}
+			
+		}
+		
 	}
 
 }
